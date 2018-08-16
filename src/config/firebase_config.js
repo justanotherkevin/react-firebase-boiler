ebc7c365746c37bdd7277e337087a32357fbbb10
kevin hu <hu.kevin.w@gmail.com>
2018-08-16 10:44:50 -0400

    Delete firebase_config.js

--- src/config/firebase_config.js
+++
@@ -1,8 +0,0 @@
-export const DB_CONFIG = {
-    apiKey: "AIzaSyBl9qp63IxNOk_3pDMPgm8wbAPqn1Q2E64",
-    authDomain: "getstarted-a3e0d.firebaseapp.com",
-    databaseURL: "https://getstarted-a3e0d.firebaseio.com",
-    projectId: "getstarted-a3e0d",
-    storageBucket: "getstarted-a3e0d.appspot.com",
-    messagingSenderId: "427783412651"
-};
